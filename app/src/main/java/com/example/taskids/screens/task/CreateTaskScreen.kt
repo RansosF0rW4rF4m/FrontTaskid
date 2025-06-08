5c2b65a281e24c860a091c385a02d1c0ff5223d8
riquelme.santos <riquelme.santos@fpf.br>
2025-06-08 15:17:54 -0300

    Reparando

--- app/src/main/java/com/example/taskids/screens/task/CreateTaskScreen.kt
+++ app/src/main/java/com/example/taskids/screens/task/CreateTaskScreen.kt
@@ -3,14 +3,12 @@ package com.example.taskids.screens.task
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,7 +23,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -86,10 +83,7 @@ fun CreateTaskScreen(
                 value = description,
                 onValueChange = { description = it },
                 label = { Text("Descrição") })
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(checked = completed, onCheckedChange = { completed = it })
-                Text("Concluída")
-            }
+
             TextField(
                 value = points,
                 onValueChange = { points = it },
