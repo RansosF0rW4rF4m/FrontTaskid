3ac13664c905d6f17af7cb0cd58861fa12ee0cbc
MariaArias <maria.216495@gmail@.com>
2025-06-09 23:50:13 -0300

    nada de click

--- app/src/main/java/com/example/taskids/screens/parent/ParentLoginScreen.kt
+++ app/src/main/java/com/example/taskids/screens/parent/ParentLoginScreen.kt
@@ -52,7 +52,7 @@ fun ParentLogin(navController: NavHostController) {
         )
 
         Text(
-            text = "Entre e mantenha o controle!",
+            text = "Entre e mantenha o controle",
             color = Color.Black,
             fontSize = 18.sp,
             fontWeight = FontWeight.Normal
@@ -98,7 +98,7 @@ fun ParentLogin(navController: NavHostController) {
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(
-            text = "não possui conta? ",
+            text = "não possui conta?",
             color = Color(0xFFE59900),
             fontSize = 14.sp
         )
